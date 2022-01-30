a3cefa41af421af353ef23ef71fc5b03eac60e84
rafet <83957086+Rafetdur@users.noreply.github.com>
2022-01-30 14:23:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+int number = 23;
+int remainder = number %2;
+//System.out.println(remainder);
+ boolean isPrime= true;
+
+ if (number==1){
+     System.out.println("Sayi Asal Degildir");
+     return;
+ }
+ if (number<1){
+     System.out.println("Gecersiz Sayi");
+ }
+
+ for (int i=2; i<number; i++) {
+     if (number % i == 0) {
+         isPrime = false;
+     }
+ }
+ if (isPrime) {
+     System.out.println("Sayi asaldir");
+ } else
+     System.out.println("Sayi asal degildir");
+
+    }
+}
